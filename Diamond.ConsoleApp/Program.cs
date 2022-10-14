578734b05fa8792c33b86ea14389cb89eeb1604f
Grzegorz Krawczyszyn <grzegorz.krawczyszyn@columbusglobal.com>
2022-10-14 15:22:01 +0200

    console.app

---
+++ Diamond.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Diamond.Core;
+
+var letters = Environment.GetCommandLineArgs()[1];
+
+var printer = new DiamondPrinter();
+try
+{
+    Console.Write(printer.Print(letters.First()));
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+}
